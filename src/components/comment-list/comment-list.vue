3e3ad524dd295deda082fd5ff1b11a909896fd68
jwchan1996 <741755613@qq.com>
2019-05-03 11:08:22 +0800

    增加评论列表组件

---
+++ src/components/comment-list/comment-list.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="comment-list">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: "我是评论列表组件"
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+    
+</style>
