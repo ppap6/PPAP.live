10caef479f2b6df13f98f842e00f636aa2edd2ed
jwchan1996 <741755613@qq.com>
2019-05-05 14:16:13 +0800

    增加搜索图标改用stylus

--- src/components/p-header/p-header.vue
+++ src/components/p-header/p-header.vue
@@ -5,7 +5,10 @@
         <router-link to="/" class="pc-logo">
           <img class="logo" src="../../assets/img/logo.png" alt>
         </router-link>
-        <input class="search-input" type="text" placeholder="搜索">
+        <div>
+          <img class="search-icon" src="../../assets/img/search.png" alt="">
+          <input class="search-input" type="text" placeholder="搜索">
+        </div> 
       </div>
       <div class="header-right">
         <ul>
@@ -31,7 +34,7 @@
 export default {};
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
 .header {
   position: fixed;
   top: 0;
@@ -41,61 +44,77 @@ export default {};
   padding: 2px 10px;
   background: #ffffff;
   box-shadow: 0 2px 10px 0 rgba(4, 21, 39, 0.2), 0 1px rgba(4, 21, 39, 0.2);
-}
 
-.container {
-  max-width: 1180px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  margin: auto;
-}
+  .container {
+    max-width: 1180px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    margin: auto;
 
-.header-left {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
+    .header-left {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
 
-.pc-logo {
-  font-size: 0;
-}
+      .pc-logo {
+        font-size: 0;
 
-.logo {
-  width: 60px;
-  height: 28px;
-  margin: 8px 10px;
-  border-radius: 5px;
-}
+        .logo {
+          width: 60px;
+          height: 28px;
+          margin: 8px 10px;
+          border-radius: 5px;
+        }
+      }
 
-.search-input {
-  height: 32px;
-  width: 240px;
-  border-radius: 10px;
-  line-height: 32px;
-  padding: 0 10px;
-  font-size: 14px;
-  background: #f2f2f2;
-}
-.search-input:hover {
-  background: #ececec;
-}
+      >div{
+        position: relative;
 
-.header-right {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
+        .search-icon {
+          position: absolute;
+          width: 20px;
+          height: 20px;
+          top: 6px;
+          left: 6px;
+          cursor: pointer;
+        }
 
-.header-right ul {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
+        .search-input {
+          height: 32px;
+          width: 240px;
+          color: #515151;
+          border-radius: 10px;
+          line-height: 32px;
+          padding: 0 10px 0 36px;
+          font-size: 14px;
+          background: #f2f2f2;
 
-.header-right ul li {
-  font-size: 14px;
-  padding: 10px;
+          &hover {
+            background: #ececec;
+          }
+        }
+       
+      }
+    }
+
+    .header-right {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      >ul {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        >li {
+          font-size: 14px;
+          padding: 10px;
+        }
+      }
+    }
+  }
 }
 </style>
