b414a28fe15281110ca3eada3ffb82ea591e52a8
jwchan1996 <741755613@qq.com>
2019-05-11 16:00:26 +0800

    完善隔个人中心的关注的话题组件

--- src/component/user/follow-topic-list/follow-topic-list.vue
+++ src/component/user/follow-topic-list/follow-topic-list.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="user-follow-topic-list">
     <h1>{{ msg }}</h1>
+    <div class="topic" v-for="item in topicList" :key="item.id">
+      <div class="name">
+        <router-link to="/topic/话题id">{{item.name}}</router-link>
+      </div>
+      <div class="follow-btn">关注</div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +14,55 @@
 export default {
   data () {
     return {
-      msg: '我的关注的话题列表组件'
+      msg: '我的关注的话题列表组件',
+      topicList: [
+        {
+          id: 8,
+          name: 'Flutter'
+        },
+        {
+          id: 9,
+          name: 'Vue'
+        },
+        {
+          id: 10,
+          name: 'React'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+.topic{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: top;
+  background-color: #FFFFFF;
+  padding: 10px 20px 10px;
+  border-radius: 5px;
+  margin: 5px 0;
 
+  .name{
+    &:hover{
+      text-decoration: underline;
+    }
+
+    a{
+      font-size: 14px;
+      color: #515151;
+      cursor: pointer;
+    }
+  }
+  
+  .follow-btn{
+    position: absolute;
+    right: 20px;
+    top: 10px;
+    font-size: 12px;
+    cursor: pointer;
+  }
+}
 </style>
