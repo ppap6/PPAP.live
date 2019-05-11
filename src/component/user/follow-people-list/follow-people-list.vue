d4c9af66d447d04be64d6b9ed5a087154bb30f5d
jwchan1996 <741755613@qq.com>
2019-05-11 15:40:53 +0800

    完善隔个人中心的关注的人组件

--- src/component/user/follow-people-list/follow-people-list.vue
+++ src/component/user/follow-people-list/follow-people-list.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="user-follow-people-list">
     <h1>{{ msg }}</h1>
+    <div class="follow" v-for="item in followList" :key="item.id">
+      <div class="left">
+        <router-link :to="`/user/${item.id}`">
+          <img class="avatar" :src="item.avatar" alt>
+        </router-link>
+      </div>
+      <div class="right">
+        <router-link :to="`/user/${item.id}`">
+          <p class="name">{{item.name}}</p>
+        </router-link>
+        <div class="count">
+          <span class="comments">评论 {{item.count.comments}}</span>
+          <span class="followers">粉丝 {{item.count.followers}}</span>
+          <span class="follows">关注用户 {{item.count.follows}}</span>
+        </div>
+      </div>
+      <div class="follow-btn">关注</div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +26,89 @@
 export default {
   data () {
     return {
-      msg: '我的关注的人列表组件'
+      msg: '我的关注列表组件',
+      followList: [
+        {
+          id: 3,
+          name: '郑宇飞',
+          avatar: 'https://img.xiaoduyu.com/FnSA2VQBP1s_T_KjDuVKxFZ8EoQp',
+          count: {
+            comments: 20,
+            followers: 1,
+            follows: 15
+          }
+        },
+        {
+          id: 4,
+          name: '层出林',
+          avatar: 'https://img.xiaoduyu.com/1528538065535-588cbeace0b53fc80c0ed551',
+          count: {
+            comments: 5,
+            followers: 5,
+            follows: 80
+          }
+        }
+      ]
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+.follow{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: top;
+  background-color: #FFFFFF;
+  padding: 10px 20px 5px;
+  border-radius: 5px;
+  margin: 5px 0;
 
+  .left {
+    .avatar {
+      height: 44px;
+      width: 44px;
+      border-radius: 50%;
+    }
+  } 
+
+  .right{
+    text-align: left;
+    padding: 2px 15px;
+
+    a {
+      .name {
+        height: 18px;
+        line-height: 18px;
+        font-size: 14px;
+        font-weight: bold;
+        color: #1c1e25 !important;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+
+    .count {
+      height: 18px;
+      line-height: 24px;
+      font-size: 12px;
+      font-weight: bold;
+
+      span{
+        margin-right: 10px;
+      }
+    }
+  }
+  
+  .follow-btn{
+    position: absolute;
+    right: 20px;
+    top: 10px;
+    font-size: 12px;
+    cursor: pointer;
+  }
+}
 </style>
