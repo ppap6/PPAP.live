c10bee3c7573cbb86bfeefd4e80376339b7408d5
jwchan1996 <741755613@qq.com>
2019-05-11 15:57:28 +0800

    完善隔个人中心的关注的帖子组件

--- src/component/user/follow-post-list/follow-post-list.vue
+++ src/component/user/follow-post-list/follow-post-list.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="user-follow-post-list">
     <h1>{{ msg }}</h1>
+    <div class="post" v-for="item in postList" :key="item.id">
+      <div class="title">
+        <router-link to="/post/帖子id">{{item.title}}</router-link>
+      </div>
+      <div class="follow-btn">关注</div>
+    </div>
   </div>
 </template>
 
@@ -8,12 +14,51 @@
 export default {
   data () {
     return {
-      msg: '我的关注的帖子列表组件'
+      msg: '我的关注的帖子列表组件',
+      postList: [
+        {
+          id: 3,
+          title: 'Flutter 1.5 发布，正式成为全平台 UI 框架'
+        },
+        {
+          id: 4,
+          title: '手摸手，带你用 vue 撸后台 系列五（v4.0 版本）'
+        }
+      ]
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+.post{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: top;
+  background-color: #FFFFFF;
+  padding: 10px 20px 10px;
+  border-radius: 5px;
+  margin: 5px 0;
 
+  .title{
+    &:hover{
+      text-decoration: underline;
+    }
+
+    a{
+      font-size: 14px;
+      color: #515151;
+      cursor: pointer;
+    }
+  }
+  
+  .follow-btn{
+    position: absolute;
+    right: 20px;
+    top: 10px;
+    font-size: 12px;
+    cursor: pointer;
+  }
+}
 </style>
