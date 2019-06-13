b1ba7f32f16f949bf5daf606254decc5af30ea4d
jwchan1996 <741755613@qq.com>
2019-06-13 19:56:12 +0800

    refactor : 调整首页布局效果

--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="container">
-    <div class="topic-list" id="left">
-      <TopicList></TopicList>
-    </div>
-    <div class="post-list" id="center">
-      <PostList :postList="postList"></PostList>
-    </div>
-    <div id="right">
+    <div id="left">
       <div class="profile-card">
         <ProfileCard></ProfileCard>
       </div>
@@ -16,6 +10,12 @@
       <div class="link-card">
         <LinkCard></LinkCard>
       </div>
+    </div>
+    <div class="post-list" id="center">
+      <PostList :postList="postList"></PostList>
+    </div>
+    <div class="topic-list" id="right">
+      <TopicList></TopicList>
     </div>
   </div>
 </template>
@@ -102,6 +102,78 @@ export default {
             likes: 1,
             collects: 0
           }
+        },
+        {
+          user_id: 1,
+          user_name: "jwchan",
+          user_avatar: "https://jwchan.cn/images/avatar.jpg",
+          topic_id: 1,
+          topic_name: "产品经理",
+          post_id: 5,
+          post_title: "这是我的第一个帖子？？？",
+          post_content: "这是这个帖子内容的一部分",
+          datetime: "02月22日",
+          count: {
+            reads: 159,
+            comments: 6,
+            answers: 2,
+            likes: 2,
+            collects: 1
+          }
+        },
+        {
+          user_id: 2,
+          user_name: "梁虚天",
+          user_avatar: "https://img.xiaoduyu.com/FklxjW19iOahN2nBRdVNvpyaPeao?imageMogr2/thumbnail/!200/quality/90",
+          topic_id: 2,
+          topic_name: "明星",
+          post_id: 6,
+          post_title: "这是我的第二个帖子？？？",
+          post_content: "这是这个帖子内容的一部分",
+          datetime: "03月22日",
+          count: {
+            reads: 19,
+            comments: 1,
+            answers: 2,
+            likes: 1,
+            collects: 0
+          }
+        },
+        {
+          user_id: 3,
+          user_name: "陆明恒",
+          user_avatar: "https://img.xiaoduyu.com/dcb97678-d958-4210-be43-6ebd5ebcc5c5.png?imageMogr2/crop/!1200x1200a593a43/thumbnail/!200/quality/90",
+          topic_id: 3,
+          topic_name: "演员",
+          post_id: 7,
+          post_title: "这是我的第三个帖子？？？",
+          post_content: "这是这个帖子内容的一部分",
+          datetime: "03月28日",
+          count: {
+            reads: 19,
+            comments: 1,
+            answers: 2,
+            likes: 0,
+            collects: 0
+          }
+        },
+        {
+          user_id: 4,
+          user_name: "周胜军",
+          user_avatar: "https://img.xiaoduyu.com/Fk2Boi_rCu0cHzXGU3v14oy8Z1sR?imageMogr2/auto-orient/thumbnail/!200/quality/90",
+          topic_id: 4,
+          topic_name: "程序员",
+          post_id: 8,
+          post_title: "这是我的第四个帖子？？？",
+          post_content: "这是这个帖子内容的一部分",
+          datetime: "04月12日",
+          count: {
+            reads: 1,
+            comments: 1,
+            answers: 0,
+            likes: 1,
+            collects: 0
+          }
         }
       ]
     }
@@ -112,6 +184,26 @@ export default {
     LinkCard,
     TopicList,
     PostList
+  },
+  mounted(){
+    this.listenScroll()
+  },
+  methods: {
+    listenScroll(){
+      window.addEventListener("scroll",this.handleScroll)
+    },
+    handleScroll(){
+      //滚动条滚动事件
+      let obtn = document.querySelector('#left');  //获取回到左边视图的ID
+      let clientHeight = document.documentElement.clientHeight;   //获取可视区域的高度
+      //获取滚动条的滚动高度
+      let osTop = document.documentElement.scrollTop || document.body.scrollTop;
+      if(osTop >= 0.55*clientHeight){  //如果滚动高度大于可视区域高度，则隐藏
+          obtn.style.display = 'none';
+      }else{         //否则显示回到顶部按钮
+          obtn.style.display = 'block';
+      }
+    }
   }
 }
 </script>
