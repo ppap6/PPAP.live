e9300f7bc3b64232eb09b43b3f56a7f51e4d35b8
jwchan1996 <741755613@qq.com>
2019-11-07 23:34:27 +0800

    feat : 封装localstorage函数

---
+++ src/common/js/localstorage.js
@@ -0,0 +1,26 @@
+export function setStorage(name, data) {
+    if (typeof data === 'object') {
+        window.localStorage.setItem(name, JSON.stringify(data))
+    } else if (typeof data === 'number' || typeof data === 'string' || typeof data === 'boolean') {
+        window.localStorage.setItem(name, data)
+    } else {
+        alert('该类型不能用于本地存储~')
+    }
+}
+
+export function getStorage(name) {
+    let data = window.localStorage.getItem(name)
+    if (data) {
+        if (data.indexOf('{') == 0) {
+            return JSON.parse(data)
+        } else {
+            return data
+        }
+    } else {
+        return ' '
+    }
+}
+
+export function removeStorage(name) {
+    window.localStorage.removeItem(name)
+}
