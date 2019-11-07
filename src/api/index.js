246e407d51f7c3fbdc62650b9da21c75a04578b0
jwchan1996 <741755613@qq.com>
2019-11-07 23:25:02 +0800

    fix : 配置axios请求

--- src/api/index.js
+++ src/api/index.js
@@ -5,16 +5,18 @@ const baseURL = 'http://localhost:2333'
 const request = axios.create({
     baseURL,
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
     }
-  });
+})
   
-  request.interceptors.request.use(
+request.interceptors.request.use(
     config => {
-      config.headers.common["Authorization"] = "Bearer " + getStorage("user").token
-      return config
+        config.headers.common["Authorization"] = "Bearer " + getStorage("user").token
+        return config
     },
     error => {
-      return Promise.reject(error)
+        return Promise.reject(error)
     }
-  );
+)
+
+export default request
