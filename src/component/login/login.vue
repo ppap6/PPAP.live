4f8469e520819dbb8de5d80c13ecd674e8f0e5ea
jwchan1996 <741755613@qq.com>
2019-11-19 23:04:27 +0800

    fix : 优化login组件

--- src/component/login/login.vue
+++ src/component/login/login.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="container">
-    <div class="header">用户登录</div>
-    <div class="main">
-      <div class="account">
-        <span>账号：</span>
-        <input type="text" v-model="account" placeholder="请输入账号……" />
-      </div>
-      <div class="password">
-        <span>密码：</span>
-        <input type="password" v-model="password" placeholder="请输入密码……" />
-      </div>
-      <div class="login">
-        <div @click="login">登录</div>
+    <div class="main-container">
+      <img class="close-btn" src="../../common/img/close.png" @click="backPrev" />
+      <div class="header">用户登录</div>
+      <div class="main">
+        <div class="account">
+          <span>账号：</span>
+          <input type="text" v-model="account" placeholder="请输入账号……" />
+        </div>
+        <div class="password">
+          <span>密码：</span>
+          <input type="password" v-model="password" placeholder="请输入密码……" />
+        </div>
+        <div class="login">
+          <div @click="login">登录</div>
+        </div>
       </div>
     </div>
   </div>
@@ -56,6 +59,9 @@ export default {
           alert(response.data.message)
         }
       })
+    },
+    backPrev(){
+      this.$router.go(-1)
     }
   }
 };
@@ -63,70 +69,89 @@ export default {
 
 <style scoped lang="stylus">
 .container {
+  width 100vw
   height calc(100vh - 80px)
   background-color #ececec
   display flex
   flex-direction column
   align-items center
   justify-content center
+  background-image url('https://jwchan.cn/images/background_header.png')
+  background-position center
+  background-repeat no-repeat
+  background-size cover
 
-  .header {
-    width 320px
-    padding 15px 20px
-    background-color #fff
-  }
-
-  .main {
-    width 320px
-    padding 0 20px 15px 20px
-    background-color #fff
+  .main-container {
+    position relative
+    border-radius 5px
     margin-bottom 150px
+    background-color rgba(255,255,255,0.92)
+    box-shadow 10px 10px 10px #ddd
 
-    .account {
-      display flex
-      align-items center
-      justify-content center
-      padding 10px 0
-      margin auto
-      // background-color #f2f2f2
-
-      input {
-        width 200px
-        padding 8px 10px
-        border 1px solid #999
-        border-radius 8px
-        font-size 14px
-      }
-    }
-
-    .password {
-      display flex
-      align-items center
-      justify-content center
-      padding 10px 0
-      margin auto
-      // background-color #f2f2f2
-
-      input {
-        width 200px
-        padding 8px 10px
-        border 1px solid #999
-        border-radius 8px
-        font-size 14px
-      }
-    }
-  }
-
-  .login {
-    div {
-      width 200px
-      margin 10px auto 8px
-      color #fff
-      padding 5px 10px
-      border-radius 20px
-      background-color #4170EA
+    .close-btn {
+      width 20px
+      height 20px
+      position absolute
+      top 10px
+      right 10px
       cursor pointer
     }
+
+    .header {
+      width 320px
+      padding 15px 20px
+    }
+
+    .main {
+      width 320px
+      padding 0 20px 15px 20px
+
+      .account {
+        display flex
+        align-items center
+        justify-content center
+        padding 10px 0
+        margin auto
+
+        input {
+          width 200px
+          padding 8px 10px
+          border 1px solid #999
+          border-radius 8px
+          font-size 14px
+        }
+      }
+
+      .password {
+        display flex
+        align-items center
+        justify-content center
+        padding 10px 0
+        margin auto
+
+        input {
+          width 200px
+          padding 8px 10px
+          border 1px solid #999
+          border-radius 8px
+          font-size 14px
+        }
+      }
+    }
+
+    .login {
+      div {
+        width 200px
+        margin 10px auto 8px
+        color #fff
+        padding 5px 10px
+        border-radius 20px
+        background-color #4170EA
+        cursor pointer
+      }
+    }
+
   }
+
 }
 </style>
