29749a6687853510e70f01e9d17a10fe3846c60f
jwchan1996 <741755613@qq.com>
2019-11-28 09:33:38 +0800

    feat : 话题帖子列表数据装载

--- src/component/topic-detail/topic-detail.vue
+++ src/component/topic-detail/topic-detail.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="topic">
-    <h1>{{ msg }}</h1>
     <div class="detail">
       <img :src="topic.icon" alt="图标">
       <div class="information">
@@ -20,86 +19,14 @@
 <script>
 import PostList from "component/post-list/post-list"
 import { getTopic } from "api/topic"
+import { getPostList } from "api/post"
 
 export default {
   data () {
     return {
       msg: '我是话题详情组件',
       topic: {},
-      postList: [
-        {
-          user_id: 1,
-          user_name: "jwchan",
-          user_avatar: "https://jwchan.cn/images/avatar.jpg",
-          topic_id: 1,
-          topic_name: "产品经理",
-          post_id: 1,
-          post_title: "这是我的第一个帖子？？？",
-          post_content: "这是这个帖子内容的一部分",
-          datetime: "02月22日",
-          count: {
-            reads: 159,
-            comments: 6,
-            answers: 2,
-            likes: 2,
-            collects: 1
-          }
-        },
-        {
-          user_id: 2,
-          user_name: "梁虚天",
-          user_avatar: "https://img.xiaoduyu.com/FklxjW19iOahN2nBRdVNvpyaPeao?imageMogr2/thumbnail/!200/quality/90",
-          topic_id: 2,
-          topic_name: "明星",
-          post_id: 2,
-          post_title: "这是我的第二个帖子？？？",
-          post_content: "这是这个帖子内容的一部分",
-          datetime: "03月22日",
-          count: {
-            reads: 19,
-            comments: 1,
-            answers: 2,
-            likes: 1,
-            collects: 0
-          }
-        },
-        {
-          user_id: 3,
-          user_name: "陆明恒",
-          user_avatar: "https://img.xiaoduyu.com/dcb97678-d958-4210-be43-6ebd5ebcc5c5.png?imageMogr2/crop/!1200x1200a593a43/thumbnail/!200/quality/90",
-          topic_id: 3,
-          topic_name: "演员",
-          post_id: 3,
-          post_title: "这是我的第三个帖子？？？",
-          post_content: "这是这个帖子内容的一部分",
-          datetime: "03月28日",
-          count: {
-            reads: 19,
-            comments: 1,
-            answers: 2,
-            likes: 0,
-            collects: 0
-          }
-        },
-        {
-          user_id: 4,
-          user_name: "周胜军",
-          user_avatar: "https://img.xiaoduyu.com/Fk2Boi_rCu0cHzXGU3v14oy8Z1sR?imageMogr2/auto-orient/thumbnail/!200/quality/90",
-          topic_id: 4,
-          topic_name: "程序员",
-          post_id: 4,
-          post_title: "这是我的第四个帖子？？？",
-          post_content: "这是这个帖子内容的一部分",
-          datetime: "04月12日",
-          count: {
-            reads: 1,
-            comments: 1,
-            answers: 0,
-            likes: 1,
-            collects: 0
-          }
-        }
-      ]
+      postList: []
     }
   },
   components: {
@@ -107,6 +34,7 @@ export default {
   },
   created(){
     this.getTopic()
+    this.getPostList()
   },
   methods: {
     getTopic(){
@@ -116,6 +44,24 @@ export default {
           this.topic = response.data.message
         }else if(response.data.status === 10003){
           this.topic = {}
+        }else{
+          console.log('服务器开小差了，请稍后重试！')
+        }
+      }).catch(error => {
+        console.log('服务器丢失了，请稍后重试！')
+      })
+    },
+    getPostList(){
+      let data = {
+        page_num: 1,
+        page_size: 20,
+        topic_id: this.$route.params.id
+      }
+      getPostList(data).then(response => {
+        if(response.data.status === 200){
+          this.postList = response.data.message
+        }else if(response.data.status === 10003){
+          this.postList = []
         }else{
           console.log('服务器开小差了，请稍后重试！')
         }
