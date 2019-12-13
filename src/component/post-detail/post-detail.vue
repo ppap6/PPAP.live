2986542e9cbc1632651c5c0d58b8839a72dd9501
jwchan1996 <741755613@qq.com>
2019-12-13 20:10:54 +0800

    style : 帖子详情富文本html样式

--- src/component/post-detail/post-detail.vue
+++ src/component/post-detail/post-detail.vue
@@ -270,12 +270,6 @@ export default {
 
     /deep/ p {
       margin 12px 0
-
-      code {
-        background-color #f2f2f2
-        padding 2px 4px
-        border-radius 4px
-      }
     }
 
     .title{
@@ -287,6 +281,56 @@ export default {
 
     .content{
       padding 30px 0 10px
+
+      /* table 样式 */
+      /deep/ table {
+        border-top: 1px solid #ccc;
+        border-left: 1px solid #ccc;
+
+        td,
+        th {
+          border-bottom: 1px solid #ccc;
+          border-right: 1px solid #ccc;
+          padding: 3px 5px;
+        }
+
+        th {
+          border-bottom: 2px solid #ccc;
+          text-align: center;
+        }
+      }
+
+      /* blockquote 样式 */
+      /deep/ blockquote {
+        display: block;
+        border-left: 8px solid #d0e5f2;
+        padding: 5px 10px;
+        margin: 10px 0;
+        line-height: 1.4;
+        font-size: 100%;
+        background-color: #f1f1f1;
+      }
+
+      /* code 样式 */
+      /deep/ code {
+        display: inline-block;
+        *display: inline;
+        *zoom: 1;
+        background-color: #f1f1f1;
+        border-radius: 3px;
+        padding: 3px 5px;
+        margin: 0 3px;
+      }
+      /deep/ pre{
+        code {
+          display: block;
+        }
+      } 
+
+      /* ul ol 样式 */
+      /deep/ ul, ol {
+        margin: 10px 0 10px 20px;
+      }
     }
   }
 
