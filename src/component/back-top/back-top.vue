f8089e7ae986f101c010f0e6c842fde45d8134a6
“jwchan1 <741755613@qq.com>
2020-04-13 01:44:57 +0800

    feat: 返回顶部组件增加文字提示

--- src/component/back-top/back-top.vue
+++ src/component/back-top/back-top.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="back-top">
-    <img id="kotori" src="~common/img/kotori.png" @click="smoothscroll">
+    <img id="kotori" src="~common/img/kotori.png" @click="smoothscroll" />
+    <p class="tips">回到顶部</p>
   </div>
 </template>
 
@@ -19,9 +20,9 @@ export default {
       let clientHeight = document.documentElement.clientHeight
       //如果滚动高度大于可视区域高度，则显示回到顶部按钮
       if(osTop >= clientHeight/3){      
-        document.querySelector('#kotori').style.display = 'block'
+        document.querySelector('#kotori').style.opacity = 1
       }else{         //否则隐藏
-        document.querySelector('#kotori').style.display = 'none'
+        document.querySelector('#kotori').style.opacity = 0
       }
     },
     smoothscroll(){
@@ -37,6 +38,33 @@ export default {
 
 <style scoped lang="stylus">
 .back-top {
+  .tips {
+    position fixed
+    bottom 80px
+    right 5px
+    cursor pointer
+    transition 0.2s all
+    font-size 14px
+    z-index 50
+    background-color #ccc
+    padding 5px 10px
+    border-radius 5px
+    opacity 0
+
+    &:hover span{
+      color red
+    }
+
+    &:after {
+      border-right 10px solid transparent
+      border-left 10px solid transparent
+      border-top 10px solid #ccc
+      position absolute
+      bottom -10px
+      right 20px
+      content ''
+    }
+  }
   #kotori {
     position fixed
     bottom -10px
@@ -45,11 +73,20 @@ export default {
     width 70px
     cursor pointer
     transition 0.2s all
-    display block
     z-index 50
+
+    &:hover {
+      transform translate(-10px, -10px)
+    }
+    &:hover +.tips {
+      opacity 1
+    }
   }
-  #kotori:hover {
-    transform translate(-10px, -10px)
+}
+
+@media screen and (max-width: 414px) {
+  .back-top {
+    display none
   }
 }
 </style>
