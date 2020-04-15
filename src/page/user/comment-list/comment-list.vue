cfbf8290201be70df1cc405f2dc138dfe970bcce
jwchan1996 <741755613@qq.com>
2020-04-15 14:57:00 +0800

    fix: 个人中心评论下拉加载更多

--- src/page/user/comment-list/comment-list.vue
+++ src/page/user/comment-list/comment-list.vue
@@ -15,30 +15,102 @@
         </div>
       </router-link>
     </div>
+    <img class="nofound" src="~common/img/404.svg" v-if="noData" />
+    <LoadingBottom :state="hasMore"></LoadingBottom>
   </div>
 </template>
 
 <script>
 import { getPersonCommentList } from 'api/person'
+import LoadingBottom from 'component/loading-bottom/loading-bottom'
+import util from 'common/js/util'
 
 export default {
   data() {
     return {
       msg: "我的回帖列表组件",
-      commentList: []
+      pageNum: 1,
+      pageSize: 20,
+      total: 0,
+      loadMoreState: false,
+      hasMore: true,
+      commentList: [],
+      noData: false
     }
   },
   created(){
     this.getPersonCommentList()
   },
+  mounted(){
+    this.listenScroll()
+  },
+  destroyed(){
+    this.removeListenScroll()
+  },
+  components: {
+    LoadingBottom
+  },
   methods: {
+    listenScroll(){
+      window.addEventListener("scroll", this.handleScroll)
+    },
+    removeListenScroll(){
+      window.removeEventListener("scroll", this.handleScroll)
+    },
+    handleScroll(){
+      let scrollDiff = util.getScrollHeight() - util.getClientHeight() - util.getScrollTop()
+      console.log(scrollDiff)
+      if(scrollDiff < 50){
+        this.loadMore()
+      }
+    },
     getPersonCommentList(){
-      let uid = this.$route.params.id
-      getPersonCommentList(uid).then(response => {
+      let data = {
+        page_num: this.pageNum,
+        page_size: this.pageSize,
+        user_id: this.$route.params.id,
+      }
+      getPersonCommentList(data).then(response => {
         if(response.data.status === 200){
+          this.total = response.data.message.total
           this.commentList = response.data.message.list
+          if(this.total < this.pageSize){
+            this.hasMore = false
+          }
         }else if(response.data.status === 10003){
           this.commentList = []
+          this.noData = true
+          this.hasMore = false
+        }else{
+          //不作处理
+          this.hasMore = false
+        }
+      }).catch(error => {
+        console.log('服务器丢失了，请稍后重试！')
+      })
+    },
+    loadMore(){
+      //判断当前是否正在加载，避免多次加载
+      if(this.loadMoreState){
+        return
+      }
+      //进入加载模式
+      this.loadMoreState = true
+      this.pageNum ++
+      let data = {
+        page_num: this.pageNum,
+        page_size: this.pageSize,
+        user_id: this.$route.params.id
+      }
+      getPersonCommentList(data).then(response => {
+        if(response.data.status === 200){
+          let list = response.data.message.list
+          for(let i=0; i<list.length; i++){
+            this.commentList.push(list[i])
+          }
+          this.loadMoreState = false
+        }else if(response.data.status === 10003){
+          this.hasMore = false
         }else{
           //不作处理
         }
@@ -133,6 +205,11 @@ export default {
         border-radius 5px
       }
     }
+  }
+
+  .nofound {
+    height 200px
+    width 200px
   }
 }
 </style>
